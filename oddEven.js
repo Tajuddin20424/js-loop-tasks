c3049d97664be9f2d83a4d2137d842cad14fa648
Tajuddin20424 <mttajuddin01@gmail.com>
2024-11-22 20:01:21 +0600

    added the all js

---
+++ oddEven.js
@@ -0,0 +1,27 @@
+
+// Subtask-1:
+
+// Find all the odd numbers from 61 to 100.
+
+let num1 = 61;
+while (num1 <= 100) {
+  if (num1 % 2 !== 0) {
+    console.log('Odd Number:', num1);
+  }
+  num1 ++ ;
+};
+
+
+// Subtask-2:
+
+// Find all the even numbers from 78 to 98.
+
+let num = 78;
+while (num <= 98) {
+  if (num % 2 === 0) {
+    console.log('Even Number:', num);
+  }
+  num ++ ;
+};
+
+
---
+++ sumEvenOdd.js
@@ -0,0 +1,38 @@
+
+// Subtask-1:
+
+// Display sum of all the odd numbers from 81 to 131.
+
+// let start = 81;
+// let end = 131;
+// let sum = 0;
+// while(start <=end){
+//     if (start % 2 !==0){
+//         sum += start;
+//     } 
+//     start ++ ;
+// }
+// console.log('sum of all odd numbers form 81 to 131 is :', sum);
+
+
+
+
+
+// Subtask-2:
+
+// Display sum of all the even numbers from 206 to 311.
+
+
+let start = 206;
+let end = 311;
+let sum = 0;
+while(start <=end){
+    if (start % 2 ===0){
+        sum += start;
+    } 
+    start ++ ;
+}
+console.log('sum of all the even numbers from 206 to 311. :', sum);
+
+
+ 
